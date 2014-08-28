a8a83b8eb6b091e17c8cce75d4553c5191dc2f82
shinjingjang <piyosasa@gmail.com>
2014-08-29 00:26:55 +0200

    Text lesen mit BufferedReader geschafft, 3 Datei(.bin/.txt/.properties) hochladen

--- src/schokolade/IO_TextDatei.java
+++ src/schokolade/IO_TextDatei.java
@@ -3,23 +3,27 @@ package schokolade;
 import java.io.*;
 
 public class IO_TextDatei {
-	
-	
+
 	BufferedReader br;
-	
-	public IO_TextDatei(){
+
+	public IO_TextDatei() {
 		textLesen();
 	}
-	
-	public void textLesen(){
-		
+
+	public void textLesen() {
+
 		try {
-			br=new BufferedReader(new FileReader("burgmann.txt"));
-			System.out.println(br);
+			br = new BufferedReader(new FileReader("burgmann.txt"));
+			String s;
+			while ((s = br.readLine()) != null) {
+				System.out.println(s);
+			}
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();;
 		}
-		
+
 	}
 }
