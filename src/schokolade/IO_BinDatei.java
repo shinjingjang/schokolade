2eecd3f63ecf8a318096772baa41c4ec78043c40
shinjingjang <piyosasa@gmail.com>
2014-08-29 20:38:50 +0200

    moechte ein Datei lesen und direkt mit gleichen Inhalte in anderem Datei speichern

---
+++ src/schokolade/IO_BinDatei.java
@@ -0,0 +1,15 @@
+package schokolade;
+
+import java.io.BufferedInputStream;
+
+public class IO_BinDatei {
+
+	BufferedInputStream bri;
+	public IO_BinDatei(){
+		dateiLesen();
+	}
+	
+	public void dateiLesen(){
+		
+	}
+}
--- src/schokolade/IO_TextDatei.java
+++ src/schokolade/IO_TextDatei.java
@@ -5,9 +5,11 @@ import java.io.*;
 public class IO_TextDatei {
 
 	BufferedReader br;
-
+	BufferedWriter bw;
+	
 	public IO_TextDatei() {
 		textLesen();
+		textschreiben2();
 	}
 
 	public void textLesen() {
@@ -19,11 +21,49 @@ public class IO_TextDatei {
 				System.out.println(s);
 			}
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();;
 		}
 
+	}
+	
+	public void textschreiben(){
+		
+		
+		try {
+			bw=new BufferedWriter(new FileWriter("new file.txt"));
+			
+			bw.write("hier ist die erste Zeile");
+		} catch (IOException e) {
+		
+			e.printStackTrace();
+		}
+		finally{
+			try {
+				bw.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	
+	public void textschreiben2(){
+		String s="lese mich";
+		String textname="text1.txt";
+		
+		FileOutputStream fo;
+		try {
+			fo = new FileOutputStream(textname);
+			for(int i=0;i<s.length();i++){
+				fo.write((byte)s.charAt(i));
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}catch(IOException e){e.printStackTrace();}
+		
 	}
 }
