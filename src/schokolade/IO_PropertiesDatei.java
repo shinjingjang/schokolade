6298fad09ed19b5375f36cc9028a399d0d6f137f
shinjingjang <piyosasa@gmail.com>
2014-08-30 15:54:49 +0200

    properties datei lesen und schreiben, geschafft

--- src/schokolade/IO_PropertiesDatei.java
+++ src/schokolade/IO_PropertiesDatei.java
@@ -1,27 +1,37 @@
 package schokolade;
 
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Properties;
 
 public class IO_PropertiesDatei {
 	
-	Properties p=new Properties();
+	Properties p=new Properties(System.getProperties());
 	
 	FileInputStream fis;
+	BufferedOutputStream fos;
+	
 	public IO_PropertiesDatei(){
-		propertiesLesen();
+		propertiesLesenSchreiben();
 	}
 	
-	public void propertiesLesen(){
+	public void propertiesLesenSchreiben(){
 		try {
 			fis =new FileInputStream(new File("burgmann.properties"));
-			
+			fos=new BufferedOutputStream(new FileOutputStream(new File("neu burgmann.properties")));
 			p.load(fis);
+			
+			//print eine bestimmte Info des Properties
 			String str = p.getProperty("plz");
 			System.out.println(str); 
+			
+			// print alle Info des Properties zu anderer neuen Datei
+			p.store(fos, "This is properties data");
+//		
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
